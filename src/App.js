5515bb9c84fb472b5effc95a1faf4f6d41c37ad3
NikHarnish <niklas.harnish@hotmail.com>
2022-02-09 10:58:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'; 
+import React, {useState} from 'react'; 
 import './App.css'; 
 import ProjectText from './ProjectText.js';
 import Menu from './Menu';
